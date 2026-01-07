421bf8c221db866c4ea4e26cbdf620b80dc1b26a
Skywalker8510 <Skywalker8510@gmail.com>
2026-01-06 19:24:14 -0600

    Error handling and feedback to user

--- src/main.rs
+++ src/main.rs
@@ -62,11 +62,29 @@ struct Teams {
     team_color: String,
 }
 
+#[derive(Debug, thiserror::Error)]
+enum ArgError {
+    #[error("the command was empty")]
+    InvalidArgument,
+    #[error("the name inputted was not found in the Scoreboard")]
+    NameNotFound,
+}
+
 fn main() {
     let args: Vec<_> = std::env::args().skip(1).collect();
     let file = std::fs::File::open(args[0].clone()).unwrap();
-    let old_name = args[1].clone();
-    let new_name = args[2].clone();
+    let old_name;
+    if let Some(o) = args.get(1) {
+        old_name = o
+    } else {
+        panic!("{:?}", ArgError::InvalidArgument);
+    }
+    let new_name;
+    if let Some(n) = args.get(2) {
+        new_name = n
+    } else {
+        panic!("{:?}", ArgError::InvalidArgument);
+    }
 
     // Player dat files are compressed with GZip.
     let mut decoder = GzDecoder::new(file);
@@ -75,19 +93,19 @@ fn main() {
 
     let mut scoreboard: ScoreboardRoot = from_bytes(&data).expect("failed to parse data");
 
-    println!("{:?}", scoreboard);
+    // println!("{:?}", scoreboard);
 
-    let old_score_data = scoreboard_for_player(scoreboard.data.player_scores.clone(), old_name.clone());
-    let new_score_data = scoreboard_for_player(scoreboard.data.player_scores.clone(), new_name.clone());
+    let old_score_data = match scoreboard_for_player(scoreboard.data.player_scores.clone(), old_name.clone()) {
+        Ok(data) => data,
+        Err(e) => panic!("{} {}", e, old_name),
+    };
     // println!("{:?}", old_score_data);
-    // println!();
-    // println!("{:?}", new_score_data);
-
-    // let old_objectives = vec![old_score_data];
-    // let newObjectives = vec![new_score_data.clone()];
-    // old_score_data.into_iter().for_each(|score| {println!("{:?}", score.objective)});
-    // println!();
-    // new_score_data.into_iter().for_each(|score| {println!("{:?}", score.objective)});
+    let new_score_data = scoreboard_for_player(scoreboard.data.player_scores.clone(), new_name.clone()).unwrap_or_else(|e| {
+        println!("{} {}", e, new_name);
+        println!("Continuing run. will replace all instances of {} with {}", old_name, new_name);
+        println!("Combining of data will not take place");
+        Vec::new()
+    });
 
     let mut updated_score_data = Vec::new();
     for data in &old_score_data {
@@ -126,10 +144,15 @@ fn main() {
         }
     }
 
-    println!("{:?}", updated_score_data);
+    // println!("{:?}", updated_score_data);
 
     let player_score_no_old_name = scoreboard_excluding_player(scoreboard.data.player_scores, old_name.clone());
-    let mut player_score_without_player = scoreboard_excluding_player(player_score_no_old_name, new_name.clone());
+    let mut player_score_without_player;
+    if !new_score_data.is_empty() {
+        player_score_without_player = scoreboard_excluding_player(player_score_no_old_name, new_name.clone());
+    } else {
+        player_score_without_player = player_score_no_old_name;
+    }
 
     player_score_without_player.extend_from_slice(&updated_score_data);
 
@@ -141,8 +164,12 @@ fn main() {
     encoder.write_all(&new_bytes).unwrap();
 }
 
-fn scoreboard_for_player(player_score: Vec<PlayerScore>, player_name: String) -> Vec<PlayerScore> {
-    player_score.into_iter().filter(|ps| ps.name == player_name).collect()
+fn scoreboard_for_player(player_score: Vec<PlayerScore>, player_name: String) -> Result<Vec<PlayerScore>, ArgError> {
+    let output: Vec<PlayerScore> = player_score.into_iter().filter(|ps| ps.name == player_name).collect();
+    if output.is_empty() {
+        return Err(ArgError::NameNotFound)
+    }
+    Ok(output)
 }
 
 fn scoreboard_excluding_player(player_score: Vec<PlayerScore>, player_name: String) -> Vec<PlayerScore> {
